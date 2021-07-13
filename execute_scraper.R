552372e9318c5dcb68b9ddeceb3697de8a9b92b4
Samara <samaravazquezperez@ada-mode.com>
2021-07-13 09:24:46 +0100

    test formatting

--- execute_scraper.R
+++ execute_scraper.R
@@ -6,12 +6,10 @@ library(rvest)
 library(purrr)
 library(aws.s3)
 
-tryCatch(
-  {
+tryCatch({
     aws.s3::s3load("find-tenderservice.RData", bucket = "tender-bot")
     aws.s3::s3load("contractsfinderservice.RData", bucket = "tender-bot")
-    aws.s3::s3load("digitalmarketplace.RData", bucket = "tender-bot")
-  },
+    aws.s3::s3load("digitalmarketplace.RData", bucket = "tender-bot")},
   error = function(e) {
     source("initialise_master_findtender.R")
     source("initialise_master_contractfinder.R")
