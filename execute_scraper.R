80e6810441fa54245785a59281c43e1067939e72
Samara <samaravazquezperez@ada-mode.com>
2021-07-13 09:17:11 +0100

    test formatting

--- execute_scraper.R
+++ execute_scraper.R
@@ -6,18 +6,14 @@ library(rvest)
 library(purrr)
 library(aws.s3)
 
-tryCatch(
-  {
+tryCatch({
     aws.s3::s3load("find-tenderservice.RData", bucket = "tender-bot")
     aws.s3::s3load("contractsfinderservice.RData", bucket = "tender-bot")
-    aws.s3::s3load("digitalmarketplace.RData", bucket = "tender-bot")
-  },
+    aws.s3::s3load("digitalmarketplace.RData", bucket = "tender-bot")},
   error = function(e) {
     source("initialise_master_findtender.R")
     source("initialise_master_contractfinder.R")
-    source("initialise_digitalmarketplace.R")
-  }
-)
+    source("initialise_digitalmarketplace.R")})
 source("gov desc web scraping.R")
 source("gov desc web scraping findtender.R")
 source("gov desc web scraping digitalmarketplace.R")
