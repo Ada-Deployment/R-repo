204113c990d653854a04d2719055294fc9ff9d82
Samara <samaravazquezperez@ada-mode.com>
2021-07-13 08:23:58 +0100

    test formatting

--- execute_scraper.R
+++ execute_scraper.R
@@ -11,11 +11,9 @@ tryCatch(
     aws.s3::s3load("find-tenderservice.RData", bucket = "tender-bot")
     aws.s3::s3load("contractsfinderservice.RData", bucket = "tender-bot")
     aws.s3::s3load("digitalmarketplace.RData", bucket = "tender-bot")
-  },
-  error = function(e) {
-    source("initialise_master_findtender.R")
-    source("initialise_master_contractfinder.R")
-    source("initialise_digitalmarketplace.R")
+  },error = function(e) {source("initialise_master_findtender.R")
+source("initialise_master_contractfinder.R")
+source("initialise_digitalmarketplace.R")
   }
 )
 source("gov desc web scraping.R")
